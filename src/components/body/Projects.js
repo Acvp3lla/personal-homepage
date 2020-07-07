563e1b284b9b88ff057edfac53cde75c44fe19a8
acvp3lla <lucinaogordon@hotmail.com>
2020-07-06 20:19:11 -0500

    Projects card updated

--- src/components/body/Projects.js
+++ src/components/body/Projects.js
@@ -1,12 +1,33 @@
 import React from 'react'
 import Styled from '../StyledComponents/Styled'
+import loading from './icons/loading.png'
+import './Project.css'
 
 function Projects(){
+
+    const style = {
+        width: "200px",
+        height: "200px",
+        position: "relative",
+        top: "20px",
+        float: "left",
+        padding: 0,
+        margin: "1rem"
+      };
+
+    const divStyle={
+        display: 'flex',
+        justifyContent: 'center'
+    }
+
     return(
         <Styled>
             <div>
                 <h1>Projects</h1>
                 <h3>...Coming Soon...</h3>
+                <div style={divStyle}>
+                    <img style={style} className="loading" src={loading} alt='loading'/>
+                </div>
             </div>
         </Styled>
     )
