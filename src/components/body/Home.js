c409162886702a7d8bf6be111964343f4865b9e8
acvp3lla <lucianogordon@hotmail.com>
2020-12-08 13:58:23 -0500

    background particles changed

--- src/components/body/Home.js
+++ src/components/body/Home.js
@@ -20,89 +20,89 @@ function Home(props){
         <div className='container-fluid d-flex justify-content-center align-items-center pt-5'>
         <Particles
             canvasClassName="particle-canvas"
+            // params={{
+            // "particles": {
+            //     "number": {
+            //         "value": 160,
+            //         "density": {
+            //             "enable": false
+            //         }
+            //     },
+            //     "size": {
+            //         "value": 10,
+            //         "random": true
+            //     },
+            //     "move": {
+            //         "direction": "bottom",
+            //         "out_mode": "out"
+            //     },
+            //     "line_linked": {
+            //         "enable": false
+            //     }
+            // },
+            // "interactivity": {
+            //     "events": {
+            //         "onclick": {
+            //             "enable": true,
+            //             "mode": "remove"
+            //         }
+            //     },
+            //     "modes": {
+            //         "remove": {
+            //             "particles_nb": 10
+            //         }
+            //     }
+            // }
             params={{
-            "particles": {
-                "number": {
-                    "value": 160,
-                    "density": {
-                        "enable": false
-                    }
-                },
-                "size": {
-                    "value": 10,
-                    "random": true
-                },
-                "move": {
-                    "direction": "bottom",
-                    "out_mode": "out"
-                },
-                "line_linked": {
-                    "enable": false
-                }
-            },
-            "interactivity": {
-                "events": {
-                    "onclick": {
-                        "enable": true,
-                        "mode": "remove"
-                    }
-                },
-                "modes": {
-                    "remove": {
-                        "particles_nb": 10
-                    }
-                }
-            }
-        //     params={{
-	    //     "particles": {
-	    //         "number": {
-	    //             "value": 160,
-	    //             "density": {
-	    //                 "enable": false
-	    //         }
-	    //     },
-	    //     "size": {
-	    //         "value": 3,
-	    //         "random": true,
-	    //         "anim": {
-	    //             "speed": 4,
-	    //             "size_min": 0.3
-	    //         }
-	    //     },
-	    //     "line_linked": {
-	    //         "enable": false
-	    //     },
-	    //     "move": {
-	    //         "random": true,
-	    //         "speed": 1,
-	    //         "direction": "top",
-	    //         "out_mode": "out"
-	    //     }
-	    // },
-	    // "interactivity": {
-	    //     "events": {
-	    //         "onhover": {
-	    //             "enable": true,
-	    //             "mode": "bubble"
-	    //         },
-	    //         "onclick": {
-	    //             "enable": true,
-	    //             "mode": "repulse"
-	    //         }
-	    //     },
-	    //     "modes": {
-	    //         "bubble": {
-	    //             "distance": 250,
-	    //             "duration": 2,
-	    //             "size": 0,
-	    //             "opacity": 0
-	    //         },
-	    //         "repulse": {
-	    //             "distance": 400,
-	    //             "duration": 4
-	    //         }
-	    //     }
-	    // }
+	        "particles": {
+	            "number": {
+	                "value": 160,
+	                "density": {
+	                    "enable": false
+	            }
+	        },
+	        "size": {
+	            "value": 3,
+	            "random": true,
+	            "anim": {
+	                "speed": 4,
+	                "size_min": 0.3
+	            }
+	        },
+	        "line_linked": {
+	            "enable": false
+	        },
+	        "move": {
+	            "random": true,
+	            "speed": 1,
+	            "direction": "top",
+	            "out_mode": "out"
+	        }
+	    },
+	    "interactivity": {
+	        "events": {
+	            "onhover": {
+	                "enable": true,
+	                "mode": "bubble"
+	            },
+	            "onclick": {
+	                "enable": true,
+	                "mode": "repulse"
+	            }
+	        },
+	        "modes": {
+	            "bubble": {
+	                "distance": 250,
+	                "duration": 2,
+	                "size": 0,
+	                "opacity": 0
+	            },
+	            "repulse": {
+	                "distance": 400,
+	                "duration": 4
+	            }
+	        }
+	    }
         }}/>
             <div className="container d-flex justify-content-center align-items-center">
                 <div className="row">
